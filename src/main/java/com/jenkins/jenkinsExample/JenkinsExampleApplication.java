a329d0b03c000a4321782b8e72058c1d4a838a7f
Mohit Kumar <mohit.kumar09@nagarro.com>
2020-07-22 17:43:12 +0530

    adding all files

---
+++ src/main/java/com/jenkins/jenkinsExample/JenkinsExampleApplication.java
@@ -0,0 +1,19 @@
+package com.jenkins.jenkinsExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExampleApplication.class, args);
+		demo();
+	}
+	public static void demo()
+	{
+		System.out.println("Hello jenkins !!!!");
+		System.out.println("Hello jenkins2 !!!!");
+		System.out.println("Hello jenkins3 !!!!");
+	}
+}
